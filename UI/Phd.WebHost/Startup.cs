e49dbd88cb3d5cc8d966903f59e566cb9d978a0d
Jeremy Pager <jpager@pulte.com>
2020-11-02 16:35:46 +0000

    Merged PR 4135: 302556 - Cache headers on static html files
    Related work items: #302556

--- UI/Phd.WebHost/Startup.cs
+++ UI/Phd.WebHost/Startup.cs
@@ -7,11 +7,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Rewrite;
+using Microsoft.AspNetCore.SpaServices;
 
 namespace Phd.WebHost
 {
     public class Startup
     {
+        private StaticFileOptions NoCacheStaticFileOptions => new StaticFileOptions
+        {
+            OnPrepareResponse = ctx =>
+            {
+                var headers = ctx.Context.Response.GetTypedHeaders();
+                headers.CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+                {
+                    Public = true,
+                    MaxAge = TimeSpan.FromDays(0)
+                };
+            }
+        };
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -37,6 +51,7 @@ namespace Phd.WebHost
                 app1.UseSpa(spa =>
                  {
                      spa.Options.DefaultPage = "/designtool/index.html";
+                     spa.Options.DefaultPageStaticFileOptions = NoCacheStaticFileOptions;
                  });
             });
 
@@ -45,6 +60,7 @@ namespace Phd.WebHost
                 app1.UseSpa(spa =>
                 {
                     spa.Options.DefaultPage = "/choiceadmin/index.html";
+                    spa.Options.DefaultPageStaticFileOptions = NoCacheStaticFileOptions;
                 });
             });
 
@@ -53,6 +69,7 @@ namespace Phd.WebHost
                 app1.UseSpa(spa =>
                 {
                     spa.Options.DefaultPage = "/salesadmin/index.html";
+                    spa.Options.DefaultPageStaticFileOptions = NoCacheStaticFileOptions;
                 });
             });
 
@@ -61,6 +78,7 @@ namespace Phd.WebHost
                 app1.UseSpa(spa =>
                 {
                     spa.Options.DefaultPage = "/salesportal/index.html";
+                    spa.Options.DefaultPageStaticFileOptions = NoCacheStaticFileOptions;
                 });
             });
 
@@ -69,6 +87,7 @@ namespace Phd.WebHost
                 app1.UseSpa(spa => 
                 {
                     spa.Options.DefaultPage = "/homedesigner/index.html";
+                    spa.Options.DefaultPageStaticFileOptions = NoCacheStaticFileOptions;
                 });
             });
         }
