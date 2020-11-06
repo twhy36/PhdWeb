6faac7dab294a5acabcf4abea96a6a6eaa4e966f
Jeremy Pager <jpager@pulte.com>
2020-11-06 20:37:24 +0000

    Merged PR 4183: Re-implement Choice Price Range on cards
    Related work items: #278686

--- UI/Phd.DesignToolWeb/ClientApp/src/app/modules/shared/pipes/priceRange.pipe.ts
+++ UI/Phd.DesignToolWeb/ClientApp/src/app/modules/shared/pipes/priceRange.pipe.ts
@@ -1,26 +1,35 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { ChoicePriceRange } from '../models/tree.model.new';
+
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import * as fromRoot from '../../ngrx-store/reducers';
+import * as fromScenario from '../../ngrx-store/scenario/reducer';
 
 @Pipe({
 	name: 'priceRange'
 })
 
-export class PriceRangePipe implements PipeTransform
-{
-	transform(values: Array<ChoicePriceRange>, choiceId: number): string
-	{
-		let range = '$0';
-		if (values) {
-			const priceRange = values.find(x => x.choiceId === choiceId);
-			if (priceRange && (priceRange.min === priceRange.max))
-			{
-				range = `\$${priceRange.min}`;
-			}
-			else if (priceRange && (priceRange.min !== priceRange.max)) {
-				range = `\$${priceRange.min} - \$${priceRange.max}`;
-			}
-		}
+export class PriceRangePipe implements PipeTransform {
+	constructor(private store: Store<fromRoot.State>) { }
 
-		return range;
+	transform(choiceId: number): Observable<string> {
+		return this.store.pipe(
+			select(fromScenario.choicePriceRangeByChoice, { choiceId }),
+			map(priceRange => {
+				let range = '$0';
+
+				if (priceRange && (priceRange.min === priceRange.max)) {
+					range = `\$${priceRange.min}`;
+				}
+				else if (priceRange && (priceRange.min !== priceRange.max)) {
+					range = `\$${priceRange.min} - \$${priceRange.max}`;
+				}
+
+				return range;
+			})
+		);
+
 	}
 }
