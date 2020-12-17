75fb172c097b1a126096f1e14716967fc87950e0
Jeremy Pager <jpager@pulte.com>
2020-12-17 15:35:50 +0000

    Merged PR 4629: add models to export
    add models to export

--- UI/projects/phd-common/src/public-api.ts
+++ UI/projects/phd-common/src/public-api.ts
@@ -2,4 +2,8 @@
  * Public API Surface of phd-common
  */
 
+export * from './lib/models/tree.model';
+export * from './lib/models/rule.model';
+export * from './lib/models/option.model';
+
 export { applyRules } from './lib/rules/rulesExecutor';
