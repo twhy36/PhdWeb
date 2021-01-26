5ad5a804df2c740b6b8668a8204ae80d1ac33437
Jeremy Pager <jpager@pulte.com>
2021-01-25 17:13:55 -0700

    migrate sales admin to new project setup

---
+++ UI/projects/salesadmin/src/app/modules/lot-managment/lot-management.module.ts
@@ -0,0 +1,75 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CalendarModule } from 'primeng/calendar';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { TableModule } from 'primeng/table';
+
+import { SharedModule } from '../shared/shared.module';
+import { PhdCommonModule } from 'phd-common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ManageHomesitesComponent } from './components/manage-homesites/manage-homesites.component';
+import { ManageHomesitesSidePanelComponent } from './components/manage-homesites-side-panel/manage-homesites-side-panel.component';
+import { PlanAssignmentComponent } from './components/plan-assignment/plan-assignment.component';
+import { PlanAssignmentSidePanelComponent } from './components/plan-assignment-side-panel/plan-assignment-side-panel.component';
+import { ReleasesComponent } from './components/releases/releases.component';
+import { ReleasesSidePanelComponent } from './components/releases-side-panel/releases-side-panel.component';
+import { LotManagementComponent } from './components/lot-management/lot-management.component';
+import { SalesProgramsComponent } from './components/sales-programs/sales-programs.component';
+import { SalesProgramsSidePanelComponent } from './components/sales-programs-side-panel/sales-programs-side-panel.component';
+import { MonotonyOptionsComponent } from './components/monotony-options/monotony-options.component';
+import { CanDeactivateGuard, ClaimGuard } from 'phd-common';
+
+const moduleRoutes: Routes = [
+    {
+        path: 'lot-management',
+        component: LotManagementComponent,
+        canActivate: [ClaimGuard],
+        data: { requiresClaim: 'SalesAdmin' },
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'manage-homesites' },
+			{ path: 'manage-homesites', component: ManageHomesitesComponent, canDeactivate: [CanDeactivateGuard]},
+			{ path: 'plan-assignment', component: PlanAssignmentComponent, canDeactivate: [CanDeactivateGuard]},
+			{ path: 'releases', component: ReleasesComponent, canDeactivate: [CanDeactivateGuard]},
+			{ path: 'sales-programs', component: SalesProgramsComponent, canDeactivate: [CanDeactivateGuard] },
+			{ path: 'monotony-options', component: MonotonyOptionsComponent }
+        ]
+    }
+];
+
+@NgModule({
+    declarations: [
+        LotManagementComponent,
+        ManageHomesitesComponent,
+        ManageHomesitesSidePanelComponent,
+        PlanAssignmentComponent,
+        PlanAssignmentSidePanelComponent,
+        ReleasesComponent,
+        ReleasesSidePanelComponent,
+        SalesProgramsComponent,
+        SalesProgramsSidePanelComponent,
+        MonotonyOptionsComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        CalendarModule,
+        ConfirmDialogModule,
+		ToastModule,
+        SharedModule,
+        PhdCommonModule,
+        TableModule,
+		NgbModule,
+        RouterModule.forChild(moduleRoutes)
+    ],
+    providers: []
+})
+export class LotManagementModule
+{
+
+}
---
+++ UI/projects/salesadmin/src/environments/environment.staging.ts
@@ -0,0 +1,28 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+    production: false,
+    apiUrl: 'https://phdapi.staging.pulte.com/odata/',
+    tenant: "pulte.onmicrosoft.com",
+	clientId: "d6e4e999-c413-4d1d-b0fb-618759cb69e5",
+	authConfig: {
+		issuer: "https://login.microsoftonline.com/1a9277a3-ef66-41f6-96b5-c5390ee468a7/v2.0",
+		clientId: "7d88528c-ab8d-4757-8b17-e7bc1b84ca15",
+		responseType: 'code',
+		clearHashAfterLogin: true,
+		requestAccessToken: true,
+		scope: 'api://7d88528c-ab8d-4757-8b17-e7bc1b84ca15/user_impersonation profile',
+		showDebugInformation: true,
+		skipIssuerCheck: false,
+		strictDiscoveryDocumentValidation: false
+	},
+    appInsights: {
+        instrumentationKey: '6fe29ada-16ba-4d15-b3a8-0364e90c4250'
+	},
+	whatFix: {
+		scriptUrl: '//whatfix.com/01caf5e0-cb2f-11e8-b979-04013d24cd02/embed/embed.nocache.js'
+	}
+};
