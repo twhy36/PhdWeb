c368b4a19b02146c94357ea0fafb5768515fdc82
Jeremy Pager <jpager@pulte.com>
2021-04-06 14:32:15 +0000

    Merged PR 5854: Bug | 316605 | change order expiration issue
    Also decided to go ahead and create a yaml pipeline for this, since it's rarely deployed
    
    Related work items: #316605

---
+++ WebJobs/ChangeOrderExpiration/EdhModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ChangeOrderExpiration
+{
+    public class SalesAgreement
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        [DataMember(Name = "status")]
+        public string Status { get; set; }
+        [DataMember(Name = "statusUtcDate")]
+        public DateTime StatusUtcDate { get; set; }
+        [DataMember(Name = "lastModifiedBy")]
+        public string LastModifiedBy { get; set; }
+        [DataMember(Name = "lastModifiedUtcDate")]
+        public DateTime LastModifiedUtcDate { get; set; }
+    }
+
+    public class JobChangeOrderGroups
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        [DataMember(Name = "salesStatusDescription")]
+        public string SalesStatusDescription { get; set; }
+        [DataMember(Name = "lastModifiedBy")]
+        public string LastModifiedBy { get; set; }
+        [DataMember(Name = "lastModifiedUtcDate")]
+        public DateTime LastModifiedUtcDate { get; set; }
+    }
+}
