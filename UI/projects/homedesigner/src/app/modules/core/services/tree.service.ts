0a5669bef60a7f4d2ebf29980fa69befd9f33dbb
Travis Green <tgreen@pulte.com>
2021-04-14 10:27:49 -0700

    #316683 Changing back to what it was.  Keeping formatting though.

--- UI/projects/homedesigner/src/app/modules/core/services/tree.service.ts
+++ UI/projects/homedesigner/src/app/modules/core/services/tree.service.ts
@@ -7,21 +7,18 @@ import { _throw } from 'rxjs/observable/throw';
 import { EMPTY as empty } from 'rxjs';
 import { of } from 'rxjs/observable/of';
 
+import
+	{
+		withSpinner, newGuid, createBatchGet, createBatchHeaders, createBatchBody,
+		IdentityService, JobChoice, ChangeOrderChoice, TreeVersionRules, OptionRule, Tree, OptionImage,
+		JobPlanOption, ChangeOrderPlanOption, PlanOptionCommunityImageAssoc
+	} from 'phd-common';
+
 import { environment } from '../../../../environments/environment';
-
-import { Tree } from '../../shared/models/tree.model';
-import { TreeVersionRules, OptionRule } from '../../shared/models/rule.model';
-
-import { OptionImage } from '../../shared/models/tree.model';
-import { withSpinner } from 'phd-common/extensions/withSpinner.extension';
-import { createBatchGet, createBatchHeaders, createBatchBody } from 'phd-common/utils/odata-utils.class';
-import { newGuid } from 'phd-common/utils/guid.class';
-import { isJobChoice } from '../../shared/classes/tree.utils';
-import { IdentityService } from 'phd-common/services';
-import { JobChoice } from '../../shared/models/job.model';
-import { ChangeOrderChoice } from '../../shared/models/job-change-order.model';
+import { isChangeOrderChoice } from '../../shared/classes/tree.utils';
 
 import * as _ from 'lodash';
+import { MyFavoritesChoice } from '../../shared/models/my-favorite.model';
 
 @Injectable()
 export class TreeService
@@ -34,12 +31,9 @@ export class TreeService
 	 * gets active tree versions for communities
 	 * @param communityIds
 	 */
-	public getTreeVersions(communityIds: Array<number>): Observable<any>
+	public getTreeVersions(planKey: number, communityId: number): Observable<any>
 	{
-		const communityFilterArray = communityIds.map(id => `dTree/plan/org/edhFinancialCommunityId eq ${id}`);
-		const communityFilter = communityFilterArray && communityFilterArray.length
-			? ` and (${communityFilterArray.join(" or ")})`
-			: '';
+		const communityFilter = ` and (dTree/plan/org/edhFinancialCommunityId eq ${communityId}) and (dTree/plan/integrationKey eq '${planKey}')`;
 
 		const entity = 'dTreeVersions';
 		const expand = `dTree($select=dTreeID;$expand=plan($select=integrationKey),org($select = edhFinancialCommunityId)),baseHouseOptions($select=planOption;$expand=planOption($select=integrationKey))`;
@@ -166,12 +160,12 @@ export class TreeService
 		);
 	}
 
-	getChoiceCatalogIds(choices: Array<JobChoice | ChangeOrderChoice>): Observable<Array<JobChoice | ChangeOrderChoice>>
+	getChoiceCatalogIds(choices: Array<JobChoice | ChangeOrderChoice | MyFavoritesChoice>): Observable<Array<JobChoice | ChangeOrderChoice>>
 	{
 		return this.identityService.token.pipe(
 			switchMap((token: string) =>
 			{
-				const choiceIds: Array<number> = choices.map(x => isJobChoice(x) ? x.dpChoiceId : x.decisionPointChoiceID);
+				const choiceIds: Array<number> = choices.map(x => isChangeOrderChoice(x) ? x.decisionPointChoiceID : x.dpChoiceId);
 				const filter = `dpChoiceID in (${choiceIds})`;
 				const select = 'dpChoiceID,divChoiceCatalogID';
 				const url = `${environment.apiUrl}dPChoices?${this._ds}filter=${encodeURIComponent(filter)}&${this._ds}select=${encodeURIComponent(select)}`;
@@ -183,17 +177,19 @@ export class TreeService
 				const newChoices = [...choices];
 				const changedChoices = [];
 				const updatedChoices = [];
+
 				if (newChoices.length > 0)
 				{
 					newChoices.forEach(c =>
 					{
-						const choiceId = isJobChoice(c) ? c.dpChoiceId : c.decisionPointChoiceID;
+						const choiceId = isChangeOrderChoice(c) ? c.decisionPointChoiceID : c.dpChoiceId;
 						const respChoice = response.value.find(r => r.dpChoiceID === choiceId);
 
 						if (respChoice)
 						{
 							changedChoices.push({ ...c, divChoiceCatalogId: respChoice.divChoiceCatalogID });
-						} else
+						}
+						else
 						{
 							changedChoices.push({ ...c });
 						}
@@ -201,12 +197,13 @@ export class TreeService
 
 					changedChoices.forEach(cc =>
 					{
-						if (isJobChoice(cc))
-						{
-							updatedChoices.push(new JobChoice(cc));
-						} else
+						if (isChangeOrderChoice(cc))
 						{
 							updatedChoices.push(new ChangeOrderChoice(cc));
+						}
+						else
+						{
+							updatedChoices.push(new JobChoice(cc));
 						}
 					});
 				}
@@ -222,7 +219,7 @@ export class TreeService
 			{
 				let guid = newGuid();
 				let requests = choices.map(choice => createBatchGet(`${environment.apiUrl}GetChoiceDetails(DPChoiceID=${choice})`));
-				let headers = createBatchHeaders(token, guid);
+				let headers = createBatchHeaders(guid, token);
 				let batch = createBatchBody(guid, requests);
 
 				return this.http.post(`${environment.apiUrl}$batch`, batch, { headers: headers });
@@ -232,6 +229,60 @@ export class TreeService
 				return response.responses.map(r => r.body);
 			})
 		);
+	}
+
+	getPlanOptionCommunityImageAssoc(options: Array<JobPlanOption | ChangeOrderPlanOption>): Observable<Array<PlanOptionCommunityImageAssoc>>
+	{
+		if (options.length)
+		{
+			return this.identityService.token.pipe(
+				switchMap((token: string) =>
+				{
+					let guid = newGuid();
+
+					let buildRequestUrl = (options: Array<JobPlanOption | ChangeOrderPlanOption>) =>
+					{
+						let optFilter = (opt: JobPlanOption | ChangeOrderPlanOption) => `planOptionCommunityId eq ${opt.planOptionId} and startDate le ${opt.outForSignatureDate} and (endDate eq null or endDate gt ${opt.outForSignatureDate})`;
+						let filter = `${options.map(opt => optFilter(opt)).join(' or ')}`;
+						let select = `planOptionCommunityId, imageUrl, startDate, endDate, sortOrder`;
+						let orderBy = `sortOrder`;
+
+						return `${environment.apiUrl}planOptionCommunityImageAssocs?${encodeURIComponent('$')}select=${select}&${encodeURIComponent('$')}filter=${filter}&${encodeURIComponent('$')}orderby=${orderBy}&${this._ds}count=true`;
+					}
+
+					const batchSize = 100;
+					let batchBundles: string[] = [];
+
+					// create a batch request with a max of 100 options per request
+					for (var x = 0; x < options.length; x = x + batchSize)
+					{
+						let optionList = options.slice(x, x + batchSize);
+
+						batchBundles.push(buildRequestUrl(optionList));
+					}
+
+					let requests = batchBundles.map(req => createBatchGet(req));
+
+					var headers = createBatchHeaders(guid, token);
+					var batch = createBatchBody(guid, requests);
+
+					return this.http.post(`${environment.apiUrl}$batch`, batch, { headers: headers });
+				}),
+				map((response: any) =>
+				{
+					let bodies: any[] = response.responses.map(r => r.body);
+
+					return bodies.map(body =>
+					{
+						// pick draft(publishStartDate is null) or latest publishStartDate(last element)
+						let value = body.value.length > 0 ? body.value[0] : null;
+
+						return value ? value as PlanOptionCommunityImageAssoc : null;
+					}).filter(res => res);
+				})
+			);
+		}
+		return of(null);
 	}
 
 	getHistoricOptionMapping(options: Array<{ optionNumber: string; dpChoiceId: number }>): Observable<{ [optionNumber: string]: OptionRule }>
@@ -299,7 +350,7 @@ export class TreeService
 				let requests = batchBundles.map(req => createBatchGet(req));
 
 				let guid = newGuid();
-				let headers = createBatchHeaders(token, guid);
+				let headers = createBatchHeaders(guid, token);
 				let batch = createBatchBody(guid, requests);
 
 				return this.http.post(`${environment.apiUrl}$batch`, batch, { headers: headers });
