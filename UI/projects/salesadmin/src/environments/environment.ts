c159a1014f873135adc9b239efb7c1161e21b863
Jeremy Pager <jpager@pulte.com>
2021-06-15 15:28:16 +0000

    Merged PR 6788: separate settings for ng serve
    baseHref in ng serve section has been deprecated, as well as outputHashing. This fixes a couple of warnings and also makes it so we don't have to remember to update environment.ts all the time.

--- UI/projects/salesadmin/src/environments/environment.ts
+++ UI/projects/salesadmin/src/environments/environment.ts
@@ -6,9 +6,7 @@
 export const environment = {
 	production: false,
 	apiUrl: 'https://phdapi.dev.pulte.com/odata/',
-	//apiUrl: 'http://localhost:2845/odata/',
 	hubUrl: 'https://phdhub.dev.pulte.com/hub',
-	//hubUrl: 'http://localhost:5000/hub',
 	tenant: "pulte.onmicrosoft.com",
 	clientId: "9d498056-e6cd-4e53-b89b-cf2f5e60adf7",
 	authConfig: {
