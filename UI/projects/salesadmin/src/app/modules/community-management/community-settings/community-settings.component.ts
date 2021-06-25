f9d2bc831a99c00a5f6a514a788cf84cc0dd4ab2
Joshua Angeley <jangeley@pulte.com>
2021-06-25 15:33:55 -0400

    Got tab switching to work

--- UI/projects/salesadmin/src/app/modules/community-management/community-settings/community-settings.component.ts
+++ UI/projects/salesadmin/src/app/modules/community-management/community-settings/community-settings.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Tabs {
+  MonotonyRules = 'Monotony Rules',
+  AutoApproval = 'Auto Approval',
+  PDF = 'PDF Upload - Additional Info.',
+  CommunitySettings = 'Community Settings'
+}
+
 @Component({
   selector: 'community-settings',
   templateUrl: './community-settings.component.html',
@@ -7,9 +14,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunitySettingsComponent implements OnInit {
 
+  public commTabs = Tabs;
+  selectedTab = this.commTabs.MonotonyRules;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onTabClick(selectedTab: Tabs) {
+    this.selectedTab = selectedTab;
   }
 
 }
