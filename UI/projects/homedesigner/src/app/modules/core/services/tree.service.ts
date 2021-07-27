094299e1c71fad2197f4e3ec5810287dc5b78c19
Jeremy Pager <jpager@pulte.com>
2021-07-27 18:53:52 +0000

    Merged PR 7329: Bug | 330344 | update planOptionCommunityImageAssocs batch size to 75
    From my testing, 92 worked but going with a lower round number for some extra wiggle room.
    
    Related work items: #330344

--- UI/projects/homedesigner/src/app/modules/core/services/tree.service.ts
+++ UI/projects/homedesigner/src/app/modules/core/services/tree.service.ts
@@ -277,7 +277,7 @@ export class TreeService
 						return `${environment.apiUrl}planOptionCommunityImageAssocs?${encodeURIComponent('$')}select=${select}&${encodeURIComponent('$')}filter=${filter}&${encodeURIComponent('$')}orderby=${orderBy}&${this._ds}count=true`;
 					}
 
-					const batchSize = 100;
+					const batchSize = 75;
 					let batchBundles: string[] = [];
 
 					// create a batch request with a max of 100 options per request
