49fda1f26565d202f0e0d33a58a8c7bfc67ae0ac
Divya Gadiraju <dgadiraju@pulte.com>
2021-08-04 21:42:59 +0000

    Merged PR 7395: Added Service to get Category/Subcategory and display on UI.
    Added Service to get Category/Subcategory and display on UI.

--- UI/projects/colormanagement/src/app/modules/core/components/search-header/colors-search-header.component.ts
+++ UI/projects/colormanagement/src/app/modules/core/components/search-header/colors-search-header.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {OptionService} from '../../services/option.service';
+import { IOptionSubCategory } from '../../../shared/models/option.model';
+import { OrganizationService } from '../../../core/services/organization.service';
+import { switchMap, startWith } from 'rxjs/operators';
+import { UnsubscribeOnDestroy } from 'phd-common';
 
 @Component({
 	selector: 'colors-search-header',
 	templateUrl: './colors-search-header.component.html',
 	styleUrls: ['./colors-search-header.component.scss']
 })
-export class ColorsSearchHeaderComponent
+export class ColorsSearchHeaderComponent extends UnsubscribeOnDestroy implements OnInit
 {
 	colorname:string;
 	isCounterVisible:boolean;
-
+	optionSubCategory:Array<IOptionSubCategory>=[];
+	currentCommunityId:number;
+	constructor(private _optionService: OptionService,private _orgService:OrganizationService) {
+		super();
+	}
+	ngOnInit() {
+		this._orgService.currentFinancialCommunity$
+		.pipe(
+			this.takeUntilDestroyed(),
+			startWith(this._orgService.currentFinancialCommunity),
+			switchMap((comm) =>
+			{
+				return this._optionService.getOptionsCategorySubcategory(comm.id)
+			})).subscribe( subCategories=>
+			{
+				this.optionSubCategory = subCategories; 					
+			});
+    }
 	showCounter(){
 		this.isCounterVisible=true;
 	}
