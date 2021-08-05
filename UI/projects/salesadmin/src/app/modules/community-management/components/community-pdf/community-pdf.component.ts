db69001ebef9a0e5962559fb0b8ddb29ddeba631
Drew Teachout <drew.teachout@slalom.come>
2021-08-05 12:26:40 -0400

    Code scaring

--- UI/projects/salesadmin/src/app/modules/community-management/components/community-pdf/community-pdf.component.ts
+++ UI/projects/salesadmin/src/app/modules/community-management/components/community-pdf/community-pdf.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { cloneDeep } from "lodash";
 import { MessageService } from "primeng/api";
