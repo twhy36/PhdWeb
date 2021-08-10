92fb2670f02b57be467d7eeed5bf9d3142c9cbbe
Jaja Harris <jharris@pulte.com>
2021-08-10 16:30:00 -0700

    create side-panel and confirm model compenents

---
+++ UI/projects/colormanagement/src/app/modules/shared/components/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+	selector: 'confirm-modal-component',
+	templateUrl: './confirm-modal.component.html',
+	styleUrls: ['./confirm-modal.component.scss']
+})
+
+export class ConfirmModalComponent
+{
+	@Input() title: string = '';
+	@Input() body: string =  '';
+	@Input() defaultOption: string = 'Continue';
+
+	constructor(public activeModal: NgbActiveModal) { }
+}
--- UI/projects/colormanagement/src/app/modules/shared/shared.module.ts
+++ UI/projects/colormanagement/src/app/modules/shared/shared.module.ts
@@ -11,6 +11,8 @@ import { PhdCommonModule } from 'phd-common';
 import { CapitalCaseSpacePipe } from './pipes/capitalCaseSpace.pipe';
 import { UnauthorizedComponent} from './components/unauthorized/unauthorized.component';
 import { CoreModule } from '../core/core.module';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { ConfirmModalComponent } from './components/confirm-modal/confirm-modal.component';
 
 
 const routes: Routes = [
@@ -20,7 +22,9 @@ const routes: Routes = [
 @NgModule({
 	declarations: [
 		UnauthorizedComponent,
-		CapitalCaseSpacePipe
+		CapitalCaseSpacePipe,
+  SidePanelComponent,
+  ConfirmModalComponent
 	],
 	imports: [
 		BrowserAnimationsModule,
