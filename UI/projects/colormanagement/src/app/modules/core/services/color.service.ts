0874e7a13a95a93ab46c80e50b8c4796b3e96a96
Divya Gadiraju <dgadiraju@pulte.com>
2021-08-23 20:47:19 +0000

    Merged PR 7750: subcategoryId = 0 case was skipped
    Related work items: #333126

--- UI/projects/colormanagement/src/app/modules/core/services/color.service.ts
+++ UI/projects/colormanagement/src/app/modules/core/services/color.service.ts
@@ -33,7 +33,7 @@ export class ColorService {
 			filter += `and (isActive eq ${isActive})`;
 		}
 
-		if (subcategoryId) {
+		if (subcategoryId >= 0) {
 			filter += `and (EdhOptionSubcategoryId eq ${subcategoryId})`;
 		}
 
