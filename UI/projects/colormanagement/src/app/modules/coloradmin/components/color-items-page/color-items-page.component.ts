7b4b4ab1ac4e7dd7bba330fc19aa5769f64a0daf
Divya Gadiraju <Divya.Gadiraju@valorepartners.com>
2021-08-27 14:14:13 -0700

    Revert this file changes.

--- UI/projects/colormanagement/src/app/modules/coloradmin/components/color-items-page/color-items-page.component.ts
+++ UI/projects/colormanagement/src/app/modules/coloradmin/components/color-items-page/color-items-page.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ColorService } from '../../../core/services/color.service';
-import { IColorItemDto } from '../../../shared/models/colorItem.model';
+import { Component } from '@angular/core'
 
 @Component({
 	selector: 'color-items-page',
 	templateUrl: './color-items-page.component.html',
 	styleUrls: ['./color-items-page.component.scss']
 })
-export class ColorItemsPageComponent implements OnInit
-{
-	list:IColorItemDto[];
+export class ColorItemsPageComponent {
 
-	constructor(private _colorService: ColorService){}
-
-	ngOnInit() 
-	{
-		let a =[31622,31766];
-		let res=this._colorService.getColorItems(8362,a).subscribe((x)=>{
-			this.list=x;
-		});
-	}
 }
