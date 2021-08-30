990bec895de5e38a9d70a2b6d2e86b8c19a93eda
Jaja Harris <jharris@pulte.com>
2021-08-30 10:03:01 -0700

    WIP

--- UI/projects/colormanagement/src/app/modules/core/services/option.service.ts
+++ UI/projects/colormanagement/src/app/modules/core/services/option.service.ts
@@ -1,96 +1,106 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, Subject, ConnectableObservable } from 'rxjs';
-import { map, catchError, publishReplay, groupBy, mergeMap, toArray, distinct } from 'rxjs/operators';
+import {
+	map,
+	catchError,
+	publishReplay,
+	groupBy,
+	mergeMap,
+	toArray,
+	distinct,
+} from 'rxjs/operators';
 import { _throw } from 'rxjs/observable/throw';
-import { IOptionSubCategory, IOptionCategory } from '../../shared/models/option.model';
+import {
+	IOptionSubCategory,
+	IOptionCategory,
+} from '../../shared/models/option.model';
 import { environment } from '../../../../environments/environment';
 import { zip } from 'rxjs';
 import { of } from 'rxjs';
-import {IColor} from '../../shared/models/color.model';
+import { IColor } from '../../shared/models/color.model';
 import { tap } from 'lodash';
 @Injectable()
-export class OptionService
-{
-    constructor(private _http: HttpClient) { }
+export class OptionService {
+	constructor(private _http: HttpClient) {}
 	private _ds: string = encodeURIComponent('$');
 
-	getOptionsCategorySubcategory(financialCommunityId: number): Observable<IOptionSubCategory[]>
-	{
+	getOptionsCategorySubcategory(
+		financialCommunityId: number
+	): Observable<IOptionSubCategory[]> {
 		const entity = `optionSubCategories`;
 		const expand = `optionCategory($select=id,name)`;
 		const filter = `optionCommunities/any(oc: oc/financialCommunityId eq ${financialCommunityId})`;
 		const select = `id,name`;
 
-		let qryStr = `${this._ds}expand=${encodeURIComponent(expand)}&${this._ds}filter=${encodeURIComponent(filter)}&${this._ds}select=${encodeURIComponent(select)}`;
+		let qryStr = `${this._ds}expand=${encodeURIComponent(expand)}&${
+			this._ds
+		}filter=${encodeURIComponent(filter)}&${
+			this._ds
+		}select=${encodeURIComponent(select)}`;
 
 		const endpoint = `${environment.apiUrl}${entity}?${qryStr}`;
 
 		return this._http.get<any>(endpoint).pipe(
-			map(response =>
-				{
-					let subCategoryList = response.value as Array<IOptionSubCategory>;
-					// sort by categoryname and then by subcategoryname
-					return subCategoryList.sort((a, b) =>
-					{
-						let aName = a.optionCategory.name.toLowerCase();
-						let bName = b.optionCategory.name.toLowerCase();
+			map((response) => {
+				let subCategoryList =
+					response.value as Array<IOptionSubCategory>;
+				// sort by categoryname and then by subcategoryname
+				return subCategoryList.sort((a, b) => {
+					let aName = a.optionCategory.name.toLowerCase();
+					let bName = b.optionCategory.name.toLowerCase();
 
-						if (aName < bName)
-						{
+					if (aName < bName) {
+						return -1;
+					}
+
+					if (aName > bName) {
+						return 1;
+					}
+
+					if ((aName = bName)) {
+						let aSubName = a.name.toLowerCase();
+						let bSubName = b.name.toLowerCase();
+
+						if (aSubName < bSubName) {
 							return -1;
 						}
 
-						if (aName > bName)
-						{
+						if (aSubName > bSubName) {
 							return 1;
 						}
 
-						if(aName = bName){
-							let aSubName = a.name.toLowerCase();
-							let bSubName = b.name.toLowerCase();
-
-							if (aSubName < bSubName)
-							{
-								return -1;
-							}
-
-							if (aSubName > bSubName)
-							{
-								return 1;
-							}
-
-							return 0;
-						}
 						return 0;
-					});
-				}),
-		catchError(this.handleError));
+					}
+					return 0;
+				});
+			}),
+			catchError(this.handleError)
+		);
 	}
 
-	private handleError(error: Response)
-	{
+	private handleError(error: Response) {
 		// In the future, we may send the server to some remote logging infrastructure.
 		console.error('Error message: ', error);
 
 		return _throw(error || 'Server error');
 	}
 
-	saveNewColors(colors: IColor[])
-	{
-		console.log('saveNewColors is executing...');
-		console.log(colors);
+	saveNewColors(colors: IColor[]): Observable<IColor[]> {
 		const endpoint = `${environment.apiUrl}colors`;
+		const body = {
+			colors: colors,
+		};
 
-		console.log(endpoint);
-		this._http.post(endpoint, colors).pipe(
-			map(response => {
-				console.log('POST - Colors Response')
+		console.log(body);
+		return this._http.post<any>(endpoint, body).pipe(
+			map((response) => {
+				console.log('POST - Colors Response');
 				console.log(response);
+				return [];
 			}),
 			catchError(this.handleError)
-
 		);
 	}
 }
