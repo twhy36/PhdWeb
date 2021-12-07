ff3a0ca1760390db116937c839c1ddcf1b956118
Jacob Ambrose <jambrose@pulte.com>
2021-12-07 15:51:58 +0000

    Merged PR 9154: Fixed spacing issue in env file
    Related work items: #340462

--- UI/projects/designpreview/src/environments/environment.qa.ts
+++ UI/projects/designpreview/src/environments/environment.qa.ts
@@ -38,6 +38,6 @@ export const environment: IEnvironment = {
 	pdfViewerBaseUrl: 'assets/pdfjs-dist/web/viewer.html',
 	brandMap: {
 		pulte: 'phd.qa.pulte.com',
-		delwebb: ' phd.qa.delwebb.com'
+		delwebb: 'phd.qa.delwebb.com'
 	}
 };
