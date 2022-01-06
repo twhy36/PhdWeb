060ead35f16f2012ea8dce7dc768a62e07db5e49
Natalie Hruby <nhruby@pulte.com>
2022-01-06 15:56:56 +0000

    Merged PR 9370: Bugfix|344936|Design Tool - Remove Design limitation to require creator to edit
    removed limitation requiring the user to have created the job change order to edit it
    
    Related work items: #344936

--- UI/projects/designtool/src/app/modules/ngrx-store/reducers.ts
+++ UI/projects/designtool/src/app/modules/ngrx-store/reducers.ts
@@ -109,9 +109,10 @@ export const canConfigure = createSelector(
 	fromChangeOrder.currentChangeOrder,
 	(scenario, market, user, sag, co) => scenario.buildMode === 'preview'
 		|| ((scenario.buildMode === 'model' || scenario.buildMode === 'spec') && !!market && user.canDesign && user.assignedMarkets && user.assignedMarkets.some(m => m.number === market.number))
-		// if there is a sales agreement, user can make changes if (a) user is at least Sales Consultant or (b) user is Design Consultant and created the current change order
+		// if there is a sales agreement, user can make changes if (a) user can Create Sales Agreements or (b) user can create Job Change Orders
 		// if the change order hasn't been saved yet, the contact field on the change order will be null
-		|| ((sag && sag.id ? (user.canSell || (user.canDesign && !!co && (!co.createdByContactId || co.createdByContactId === user.contactId))) : user.canConfigure) && !!market && user.assignedMarkets && user.assignedMarkets.some(m => m.number === market.number))
+		|| ((sag && sag.id ? (user.canSell || (user.canDesign && !!co && (!co.createdByContactId))) : user.canConfigure) 
+		&& !!market && user.assignedMarkets && user.assignedMarkets.some(m => m.number === market.number))
 )
 
 export const canSell = createSelector(
