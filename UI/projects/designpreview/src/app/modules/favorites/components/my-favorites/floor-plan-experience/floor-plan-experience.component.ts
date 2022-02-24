0a96ce08e3cf13521360fdbe83312e366269f335
Abinay John <ajohn@pulte.com>
2022-02-24 08:28:04 -0500

    Added detection for the decision point side bar

--- UI/projects/designpreview/src/app/modules/favorites/components/my-favorites/floor-plan-experience/floor-plan-experience.component.ts
+++ UI/projects/designpreview/src/app/modules/favorites/components/my-favorites/floor-plan-experience/floor-plan-experience.component.ts
@@ -163,10 +163,12 @@ export class FloorPlanExperienceComponent extends UnsubscribeOnDestroy implement
 
 	scrollPointIntoView(pointId: number)
 	{
-		const decisionBarElement = document.getElementById('decision-bar-' + pointId?.toString());
+		const decisionBarElement = <HTMLElement><any>document.getElementById('decision-bar-' + pointId?.toString());
 		if (decisionBarElement)
 		{
+			setTimeout(() => {
 				decisionBarElement.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+			}, 0);
 		}
 	}
 }
