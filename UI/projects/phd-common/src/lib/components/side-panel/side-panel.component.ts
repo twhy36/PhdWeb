c78bb7a9d8f7af2ff5cd81ae5cd4b54bb8fa8c8c
Travis Green <Travis.Green@PulteGroup.com>
2022-06-13 20:08:46 +0000

    Merged PR 11967: WI|364047|Issue: SA - Manage Homesite - Screen not locked down when Homesite Details (left slide out) is open
    #364047 Fixed issue with scrollbars and side panel overlay not displaying correctly
    
    Related work items: #364047

--- UI/projects/phd-common/src/lib/components/side-panel/side-panel.component.ts
+++ UI/projects/phd-common/src/lib/components/side-panel/side-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, TemplateRef, Output, EventEmitter } from '@angular/core';
+import { Component, Input, TemplateRef, Output, EventEmitter, OnDestroy } from '@angular/core';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -11,31 +11,70 @@ import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component'
 	styleUrls: ['./side-panel.component.scss']
 })
 
-export class SidePanelComponent {
+export class SidePanelComponent implements OnDestroy
+{
 	@Output() onSidePanelClose = new EventEmitter<boolean>();
 	@Output() onSidePanelConfirmed = new EventEmitter<boolean>();
+
 	@Input() sidePanelOpen: boolean = false;
 	@Input() customClasses: string = '';
-
 	@Input() headerTemplate: TemplateRef<any>;
 	@Input() subheaderTemplate: TemplateRef<any>;
 	@Input() bodyTemplate: TemplateRef<any>;
 	@Input() footerTemplate: TemplateRef<any>;
-  @Input() isDirty: boolean;
+	@Input() isDirty: boolean;
 	@Input() customMsgBody: string;
 
-	constructor(private _modalService: NgbModal) { }
+	constructor(private _modalService: NgbModal)
+	{
+		this.createOverlay();
+	}
 
-	toggleSidePanel() {
-		if (this.isDirty) {
+	ngOnDestroy()
+	{
+		this.removeOverlay();
+	}
+
+	toggleSidePanel()
+	{
+		if (this.isDirty)
+		{
 			this.showNavAway();
 		}
-		else {
+		else
+		{
 			this.onSidePanelClose.emit(!this.sidePanelOpen);
+			
 		}
 	}
 
-  showNavAway() {
+	createOverlay()
+	{
+		var overlayDiv: HTMLElement = document.getElementById('phd-side-panel-overlay');
+
+		if (!overlayDiv)
+		{
+			overlayDiv = document.createElement('div');
+
+			overlayDiv.id = 'phd-side-panel-overlay';
+			overlayDiv.className = 'phd-side-panel-overlay';
+
+			document.body.appendChild(overlayDiv);
+		}
+	}
+
+	removeOverlay()
+	{
+		var overlayDiv = document.getElementById('phd-side-panel-overlay');
+
+		if (overlayDiv)
+		{ 
+			overlayDiv.remove();
+		}
+	}
+
+	showNavAway()
+	{
 		let msgBody = `If you continue you will lose your changes.<br><br> `;
 		msgBody += `Do you wish to continue?`;
 
@@ -45,24 +84,29 @@ export class SidePanelComponent {
 		confirm.componentInstance.body = msgBody;
 		confirm.componentInstance.defaultOption = 'Cancel';
 
-		confirm.result.then((result) => {
-			if (result == 'Continue') {
+		confirm.result.then((result) =>
+		{
+			if (result == 'Continue')
+			{
 				this.onSidePanelClose.emit(!this.sidePanelOpen);
 			}
-		}, (reason) => {} );  
+		}, (reason) => { });
 	}
 
-	showCustomConfirm() {
+	showCustomConfirm()
+	{
 		let confirm = this._modalService.open(ConfirmModalComponent, { centered: true });
 
 		confirm.componentInstance.title = 'Attention!';
 		confirm.componentInstance.body = this.customMsgBody;
 		confirm.componentInstance.defaultOption = 'Cancel';
 
-		confirm.result.then((result) => {
-			if (result == 'Continue') {
+		confirm.result.then((result) =>
+		{
+			if (result == 'Continue')
+			{
 				this.onSidePanelConfirmed.emit(!this.sidePanelOpen);
 			}
-		}, (reason) => {} );  
+		}, (reason) => { });
 	}
 }
