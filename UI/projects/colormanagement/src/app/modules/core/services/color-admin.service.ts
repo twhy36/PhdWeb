0b032133819678dbfa69be39a2e4d0bafbd9325e
Jaja Harris <jharris@pulte.com>
2022-07-15 21:58:30 +0000

    Merged PR 12801: WI | 366206 | Hide or show Option Packages tab based on related feature switch
    Related work items: #366206

--- UI/projects/colormanagement/src/app/modules/core/services/color-admin.service.ts
+++ UI/projects/colormanagement/src/app/modules/core/services/color-admin.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import {ReplaySubject} from 'rxjs';
+import {combineLatest, of, ReplaySubject} from 'rxjs';
+import {filter, switchMap} from 'rxjs/operators';
+import {OrganizationService} from './organization.service';
+import {FeatureSwitchService} from 'phd-common';
 
 @Injectable()
 export class ColorAdminService {
+	constructor(
+		private _featureSwitchService: FeatureSwitchService,
+		private _orgService: OrganizationService) { }
 
 	private editingColorSource = new ReplaySubject<boolean>(1);
 	editingColor$ = this.editingColorSource.asObservable();
@@ -10,4 +16,24 @@ export class ColorAdminService {
 	emitEditingColor(isVisible: boolean) {
 		this.editingColorSource.next(isVisible);
 	}
+
+	optionPackagesEnabled$ = combineLatest([
+		this._orgService.currentCommunity$.pipe(
+			filter((comm) => !!comm),
+		),
+		this._orgService.currentMarket$.pipe(
+			filter((market) => !!market),
+		)]
+	)
+		.pipe(
+			switchMap(([financialCommunity, market]) => {
+				if (!financialCommunity)
+				{
+					return of(true);
+				}
+
+				const org = { edhMarketId: market.id, edhFinancialCommunityId: financialCommunity.id };
+				return this._featureSwitchService.isFeatureEnabled('Option Packages', org)
+			})
+		)
 }
