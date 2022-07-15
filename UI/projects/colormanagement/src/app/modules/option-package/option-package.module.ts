e99220bf43df46c732d49a8d3ce86b8b85409f67
Christopher Taleck <ctaleck@pulte.com>
2022-07-15 15:55:49 +0000

    Merged PR 12786: WI | 344453 | Color Mgmt - Option Package Admin - Add Option Package (WIP)
    - Add dialog and form
    - Move main logic from "header" component to "page" component
    - TODO : Saving functionality
    
    Related work items: #344453

--- UI/projects/colormanagement/src/app/modules/option-package/option-package.module.ts
+++ UI/projects/colormanagement/src/app/modules/option-package/option-package.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { OptionPackageRoutingModule } from './option-package-routing.module';
 import { OptionPackagesPageComponent } from './components/option-packages-page/option-packages-page.component';
 import { OptionPackagesHeaderComponent } from './components/option-packages-header/option-packages-header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EditOptionPackagesComponent } from './components/edit-option-packages/edit-option-packages.component';
-import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { PhdCommonModule } from 'phd-common';
 import { SharedModule } from 'primeng/api';
 import { CoreModule } from '../core/core.module';
+import { AddDialogComponent } from './components/add-dialog/add-dialog.component';
 import { MultiSelectModule } from 'primeng/multiselect';
 
 
@@ -18,6 +19,7 @@ import { MultiSelectModule } from 'primeng/multiselect';
 	declarations: [
 		OptionPackagesPageComponent,
 		OptionPackagesHeaderComponent,
+		AddDialogComponent,
 		EditOptionPackagesComponent
 	],
 	imports: [
@@ -26,6 +28,7 @@ import { MultiSelectModule } from 'primeng/multiselect';
 		NgbModule,
 		CommonModule,
 		FormsModule,
+		ReactiveFormsModule,
 		CoreModule,
 		SharedModule,
 		InfiniteScrollModule,
