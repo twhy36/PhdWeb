9c46bb928eda1ca0d42ad0de691a6ddbe4b92f09
keegan mcgonigle <kmcgonigle@pulte.com>
2022-07-28 20:18:04 -0700

    Added OptionPackage option model

--- UI/projects/colormanagement/src/app/modules/shared/models/option.model.ts
+++ UI/projects/colormanagement/src/app/modules/shared/models/option.model.ts
@@ -17,6 +17,30 @@ export interface IOptionCommunity
   optionSubCategory:IOptionSubCategory
 }
 
+export interface OptionPackageListItemDto 
+{
+    id: number;
+    financialCommunityId: number;
+    optionSubCategoryId: number;
+    optionSalesName: string;
+    planOptionCommunities:
+    {
+        id: number;
+        planId: number;
+        maxOrderQty: number;
+    }[];
+    optionSubCategory: 
+    {
+        name:string;
+        id:number;
+        optionCategory: 
+        {
+            id: number;
+            name: string;
+          }
+    };
+}
+
 //This is for the dummy attribute group
 export enum AttributeGroupKey
 {
