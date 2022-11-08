039a2d9bad9fa19e03cf8f762769d85c88133303
Christopher Taleck <ctaleck@pulte.com>
2022-11-08 16:43:41 +0000

    Merged PR 15711: WI | 380965 | Issue - Create Envelope red toast err when tried to view CO after it was approved
    Make sure att id is not null
    
    Related work items: #380965

--- UI/projects/designtool/src/app/modules/shared/classes/contract-utils.ts
+++ UI/projects/designtool/src/app/modules/shared/classes/contract-utils.ts
@@ -355,7 +355,7 @@ export function getLiteChangeOrderGroupSelections(
 				{
 					coPlanOption.jobChangeOrderPlanOptionAttributes.map(att =>
 					{
-						const colorSchemeChoice = createLiteSDChoice(att.attributeName, att.id, null, 0, 1);
+						const colorSchemeChoice = createLiteSDChoice(att.attributeName, att.id ?? 0, null, 0, 1);
 						colorSchemeChoices.push(colorSchemeChoice);
 					});
 				}
@@ -507,7 +507,7 @@ export function getLiteConstructionChangeOrderPdfData(
 							options: [],
 							overrideNote: null,
 							dpChoiceId: 0,
-							divChoiceCatalogId: att.id, // used for option filtering in API
+							divChoiceCatalogId: att.id ?? 0, // used for option filtering in API
 							attributes: [],
 							action: att.action
 						});
