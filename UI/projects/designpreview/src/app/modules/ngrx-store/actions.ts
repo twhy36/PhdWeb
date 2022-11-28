ac60aa2a460eeb2bf0e16d0e1e0e92a69d6058ef
Drew Teachout <dteachout@pulte.com>
2022-11-28 20:16:47 +0000

    Merged PR 16117: WI | 380638 | Split Batch Requests into Individual HTTP Requests
    Related work items: #380638, #382196

--- UI/projects/designpreview/src/app/modules/ngrx-store/actions.ts
+++ UI/projects/designpreview/src/app/modules/ngrx-store/actions.ts
@@ -1,20 +1,22 @@
 import { Action } from '@ngrx/store';
 
-import {
-	SalesCommunity, Job, JobChoice, ChangeOrderGroup, ChangeOrderHanding, LotExt, PlanOption, 
+import
+{
+	SalesCommunity, Job, JobChoice, ChangeOrderGroup, ChangeOrderHanding, LotExt, PlanOption,
 	TreeVersionRules, SalesAgreement, Tree, OptionImage, SalesAgreementInfo, MyFavorite, DesignToolAttribute,
 	MyFavoritesChoice
 } from 'phd-common';
 
-import { ErrorAction, ErrorFrom } from './error.action';
+import { ErrorAction } from './error.action';
 import { Stopwatch } from './stopwatch';
 
-export enum CommonActionTypes {
-    LoadSalesAgreement = 'Load Sales Agreement',
+export enum CommonActionTypes
+{
+	LoadSalesAgreement = 'Load Sales Agreement',
 	SalesAgreementLoaded = 'Sales Agreement Loaded',
 	ResetFavorites = 'Reset Favorites',
 	MyFavoritesChoiceAttributesDeleted = 'My Favorites Choices Attributes Deleted',
-    LoadError = 'Load Error',
+	LoadError = 'Load Error',
 	PageNotFound = 'Page Not Found',
 	SetLatestError = 'Set Latest Error',
 	ClearLatestError = 'Clear Latest Error'
@@ -26,8 +28,8 @@ export class LoadSalesAgreement implements Action
 	readonly type = CommonActionTypes.LoadSalesAgreement;
 
 	constructor(
-		public salesAgreementId: number, 
-		public clearState: boolean = true, 
+		public salesAgreementId: number,
+		public clearState: boolean = true,
 		public isBuyerPreview: boolean = false
 	) { }
 }
@@ -48,33 +50,34 @@ export class SalesAgreementLoaded implements Action
 		public rules: TreeVersionRules,
 		public options: PlanOption[],
 		public optionImages: OptionImage[],
+		public webPlanMappings: number[],
 		public changeOrder: ChangeOrderGroup,
 		public lot: LotExt,
 		public myFavorites: MyFavorite[]
 	)
-	{}
+	{ }
 }
 
 export class ResetFavorites implements Action
 {
 	readonly type = CommonActionTypes.ResetFavorites;
 
-	constructor() {	}
+	constructor() { }
 }
 
 export class MyFavoritesChoiceAttributesDeleted implements Action
 {
 	readonly type = CommonActionTypes.MyFavoritesChoiceAttributesDeleted;
 
-	constructor(public attributes: DesignToolAttribute[], public locations: DesignToolAttribute[], public myFavoritesChoice: MyFavoritesChoice) {	}
+	constructor(public attributes: DesignToolAttribute[], public locations: DesignToolAttribute[], public myFavoritesChoice: MyFavoritesChoice) { }
 }
 
 export class LoadError extends ErrorAction
 {
 	readonly type = CommonActionTypes.LoadError;
 
-	constructor(public error: Error, 
+	constructor(public error: Error,
 		public friendlyMessage?: string,
-		public errFrom?: string) 
+		public errFrom?: string)
 	{ super(error, friendlyMessage, errFrom); }
 }
