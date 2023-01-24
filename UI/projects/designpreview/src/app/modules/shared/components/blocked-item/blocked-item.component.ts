eac80e7019f8933f3f0914a660390b8dff2d298f
Linda Liu <lliu@pulte.com>
2023-01-24 19:09:37 +0000

    Merged PR 17255: DP | 385669 | blocked point nav issue
    blocked point nav fix for left nav panel
    
    Related work items: #385669

--- UI/projects/designpreview/src/app/modules/shared/components/blocked-item/blocked-item.component.ts
+++ UI/projects/designpreview/src/app/modules/shared/components/blocked-item/blocked-item.component.ts
@@ -10,11 +10,12 @@ import _ from 'lodash';
 import * as NavActions from '../../../ngrx-store/nav/actions';
 
 @Component({
-  selector: 'blocked-item',
-  templateUrl: './blocked-item.component.html',
-  styleUrls: ['./blocked-item.component.scss']
+	selector: 'blocked-item',
+	templateUrl: './blocked-item.component.html',
+	styleUrls: ['./blocked-item.component.scss']
 })
-export class BlockedItemComponent  extends UnsubscribeOnDestroy implements OnInit {
+export class BlockedItemComponent extends UnsubscribeOnDestroy implements OnInit
+{
 	@Input() disabledByItem: BlockedByItem;
 	@Input() isChoiceItem: boolean;
 	@Input() conjunction: string;
@@ -25,33 +26,52 @@ export class BlockedItemComponent  extends UnsubscribeOnDestroy implements OnIni
 	myFavoriteId: number;
 	filteredTree: TreeVersion;
 	subGroups: SubGroup[];
+	currentSelectedPointId: number;
 
 
 	constructor(private store: Store<fromRoot.State>, private router: Router) { super() }
 
-	ngOnInit(): void {
-		this.isHiddenChoiceItem = this.hiddenChoices.some(choice => choice.id === this.disabledByItem.choiceId);
-		
+	ngOnInit(): void
+	{
 		this.store.pipe(
 			this.takeUntilDestroyed(),
 			select(fromFavorite.currentMyFavorite),
-			withLatestFrom(this.store.pipe(select(fromRoot.filteredTree)))
-			).subscribe(([fav, tree]) => {
-				this.myFavoriteId = fav && fav.id;
-				this.filteredTree = tree;
-				this.subGroups = _.flatMap(tree.groups, g => _.flatMap(g.subGroups)) || [];
+			withLatestFrom(
+				this.store.pipe(select(fromRoot.filteredTree)),
+				this.store.pipe(select(state => state.nav))
+			)
+		).subscribe(([fav, tree, nav]) =>
+		{
+			this.myFavoriteId = fav && fav.id;
+			this.filteredTree = tree;
+			this.subGroups = _.flatMap(tree.groups, g => _.flatMap(g.subGroups)) || [];
+			this.currentSelectedPointId = nav.selectedPoint;
 		});
+
+		this.isHiddenChoiceItem = this.hiddenChoices.some(choice => choice.id === this.disabledByItem.choiceId);
 	}
 
-	onBlockedItemClick(pointId: number) {
+	onBlockedItemClick(pointId: number)
+	{
 		this.blockedItemClick.emit();
 		const subGroup = this.subGroups.find(sg => !!sg.points.find(p => p.id === pointId))
 
 		this.store.dispatch(new NavActions.SetSelectedSubgroup(subGroup.id, pointId, null));
-		this.router.navigate(['favorites', 'my-favorites', this.myFavoriteId, subGroup.subGroupCatalogId], { queryParams: { presale: sessionStorage.getItem('presale_token')} })
+		this.router.navigate(['favorites', 'my-favorites', this.myFavoriteId, subGroup.subGroupCatalogId], { queryParams: { presale: sessionStorage.getItem('presale_token') } })
+
+		//Scroll to selected blocked point if same blocked point selected and change event not triggered
+		if (this.currentSelectedPointId && this.currentSelectedPointId === pointId)
+		{
+			const decisionBarElement = <HTMLElement><any>document.getElementById('decision-bar-' + pointId?.toString());
+			if (decisionBarElement)
+			{
+				decisionBarElement.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'nearest' });
+			}
+		}
 	}
 
-	displayBlockedItems() {
+	displayBlockedItems()
+	{
 		return (this.isHiddenChoiceItem || this.disabledByItem.pointId === null) ? 'phd-hidden-item' : 'phd-clickable phd-blocked-link'
 	}
 
