cff9fcbbac132f5ff39a724798fc7463ef900729
Abinay John <ajohn@pulte.com>
2023-02-15 14:54:27 +0000

    Merged PR 17952: WI | 387847 | DP Presale - Summary Page Floorplan
    - New presale specific dislaimer
    - Header says "FLOORPLAN IMAGE" for both pre-sale AND post-contract
    
    Related work items: #387847

--- UI/projects/designpreview/src/app/modules/favorites/components/favorites-summary/favorites-summary.component.ts
+++ UI/projects/designpreview/src/app/modules/favorites/components/favorites-summary/favorites-summary.component.ts
@@ -93,6 +93,13 @@ export class FavoritesSummaryComponent extends UnsubscribeOnDestroy implements O
 		return "This Design Preview is a tool designed to give our customers a general understanding of home options, material and finish upgrades and option/upgrade pricing (where provided) and prepare them for making actual option and upgrade selections in the future. No selections are being made using this tool, nor is this a contract for a home or reservation of a lot. The terms and conditions pertaining to a home purchase, including option and upgrade selections, will be contained only within a fully-executed Home Purchase Agreement or a Change Order to that agreement. Lots, home plans, elevations, options, upgrades, features and specifications and the availability and pricing of each may change without notice. Images are for marketing purposes only and may not reflect exact home designs or dimensions, specific components or materials used in home construction, specific manufacturer or models of components, or exact colors or textures of materials, all of which may vary in the course of actual construction and all of which seller has the right to change. Model homes may vary significantly in design, décor and available options and materials from homes available to purchase in a community.";
 	}
 
+	get floorPlanDisclaimer()
+	{
+		return this.isPresale 
+			? '*Floorplans are for illustrative purposes only and may differ from actual available floor plans and actual features and measurements of completed home.'
+			: '*Floorplan may show options you selected as well as previously contracted options. Floorplans are for illustrative purposes only and may differ from actual available floor plans and actual features and measurements of completed home.';
+	}
+
 	ngOnInit()
 	{
 		this.activatedRoute.paramMap
