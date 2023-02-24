86f425e934e305d90a21a1d139142bd5e452206e
Sean Scott <sscott@pulte.com>
2023-02-24 15:34:08 +0000

    Merged PR 18211: WI | 388662 | DT: Pricing issue due to Time of Sale record being auto-saved
    Added a listener to the `beforeunload` event to delete auto-saved TOS records once the browser is closed
    
    Related work items: #388662

--- UI/projects/designtool/src/app/app.component.ts
+++ UI/projects/designtool/src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 import { filter } from 'rxjs/operators';
 
-import { loadScript } from 'phd-common';
+import { loadScript, TimeOfSaleOptionPrice, UnsubscribeOnDestroy } from 'phd-common';
+
 import { environment } from '../environments/environment';
 import { default as build } from './build.json';
 import { NotificationService } from './modules/core/services/notification.service';
+import { JobService } from './modules/core/services/job.service';
+import { select, Store } from '@ngrx/store';
+import * as fromRoot from './modules/ngrx-store/reducers';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent
+export class AppComponent extends UnsubscribeOnDestroy implements OnDestroy 
 {
 	environment = environment;
+	timeOfSaleOptionPrices: TimeOfSaleOptionPrice[];
 
 	get branch(): string
 	{
@@ -27,8 +32,14 @@ export class AppComponent
 		return build.version;
 	}
 
-	constructor(private router: Router, private notificationService: NotificationService)
+	constructor(
+		private router: Router,
+		private notificationService: NotificationService,
+		private jobService: JobService,
+		private store: Store<fromRoot.State>)
 	{
+		super();
+
 		this.router.events.pipe(
 			filter(evt => evt instanceof NavigationEnd)
 		).subscribe(() =>
@@ -43,5 +54,26 @@ export class AppComponent
 
 		this.notificationService.init();
 		this.notificationService.registerHandlers();
+
+		this.store.pipe(
+			this.takeUntilDestroyed(),
+			select(store => store.job)
+		).subscribe(job => 
+		{
+			this.timeOfSaleOptionPrices = job?.timeOfSaleOptionPrices;
+		});
+	}
+
+	// #388662
+	// When the browser is closed, we must remove any auto-saved TimeOfSale prices
+	// that are attached to an active, unapproved CO. The API logic will automatically 
+	// determine what prices to delete based on the latest CO.
+	@HostListener('window:beforeunload')
+	async ngOnDestroy()
+	{
+		if (this.timeOfSaleOptionPrices?.length)
+		{
+			await this.jobService.deleteTimeOfSaleOptionPrices(this.timeOfSaleOptionPrices, true).toPromise();
+		}
 	}
 }
